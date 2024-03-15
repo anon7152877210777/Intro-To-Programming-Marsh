3d3a5f8b957d2d7af1c31aca5960677613bf61a8
unbloxed <104168273+artyomriseofnations@users.noreply.github.com>
2024-03-15 07:58:47 -0500

    Create 3.5.8 Rectangle, Part 3.py

---
+++ Lessons 3.1 through 3.6/3.5.8 Rectangle, Part 3.py
@@ -0,0 +1,8 @@
+length = int(input("Whats your length?: "))
+width = int(input("Whats your width?: "))
+
+area = length * width
+perimeter = length + length + width + width
+
+print("Area:" + str(area))
+print("Perimeter:" + str(perimeter))
