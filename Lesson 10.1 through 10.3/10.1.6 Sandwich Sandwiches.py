da1340ed96bf16ed6a8115d5bf56243910298e02
12345 <104168273+anon7152877210777@users.noreply.github.com>
2024-04-29 13:45:12 -0500

    Create 10.1.6 Sandwich Sandwiches.py

---
+++ Lesson 10.1 through 10.3/10.1.6 Sandwich Sandwiches.py
@@ -0,0 +1,7 @@
+# update the function's body to return the 
+# first and last letter of the input concatenated together
+def sandwich(s):
+    return s[0] + s[-1]
+
+print(sandwich("pbj")) 
+print(sandwich("blt"))
