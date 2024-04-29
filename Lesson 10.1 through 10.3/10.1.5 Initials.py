ef39f126aff7365784afb8998dd34cd4a5826da8
12345 <104168273+anon7152877210777@users.noreply.github.com>
2024-04-29 13:38:24 -0500

    Create 10.1.5 Initials.py

---
+++ Lesson 10.1 through 10.3/10.1.5 Initials.py
@@ -0,0 +1,6 @@
+# fill in the body of this function!
+def initials(first, last):
+    fi = first[0].upper()
+    li = last[0].upper()
+    return first[0] + '.' + last[0] + '.'
+print(initials("First", "Last"))
