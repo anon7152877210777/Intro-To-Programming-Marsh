007c937058fd56f1220bd4183099bc70085e389b
12345 <104168273+anon7152877210777@users.noreply.github.com>
2024-05-02 14:28:53 -0500

    Create 10.6.4 ENTHUSIASM!.py

---
+++ Lesson 10.6/10.6.4 ENTHUSIASM!.py
@@ -0,0 +1,6 @@
+# return the input capitalized and with an exclamation point!
+def add_enthusiasm(string):
+    return string.upper() + "!"
+
+print(add_enthusiasm("hi"))
+print(add_enthusiasm("Goodbye. See you later."))
